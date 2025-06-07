1c7389a295f375e44600c337e9bd13bec35a2ee9
Aregomz <223728@ids.upchiapas.edu.mx>
2025-06-07 16:06:29 -0600

    Feature:cv completed: 5%/100%

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+import './App.css'
+
+export default function App() {
+  return (
+    <div className="h-screen bg-gradient-to-br from-purple-700 to-black flex items-center justify-center text-white text-4xl font-bold">
+      ¡Hola, soy Arell y este es mi CV!
+    </div>
+  );
+}
