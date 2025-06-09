186d76dc1ba103f0c85b5685e491efc4250f1ddc
Aregomz <223728@ids.upchiapas.edu.mx>
2025-06-08 19:17:42 -0600

    pequeño cambio

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import './App.css'
 export default function App() {
   return (
     <div className="h-screen bg-gradient-to-br from-purple-700 to-black flex items-center justify-center text-white text-4xl font-bold">
-      ¡Hola, soy Arell y este es mi CV!
+      ¡Hola, Bienvenido a mi portafolio!
     </div>
   );
 }
