5ff1419a877b293ef882140b6745e0feebed41cd
Aregomz <223728@ids.upchiapas.edu.mx>
2025-06-09 21:12:02 -0600

    little

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import './App.css'
 export default function App() {
   return (
     <div className="h-screen bg-gradient-to-br from-purple-700 to-black flex items-center justify-center text-white text-4xl font-bold">
-      ¡Hola, Bienvenido a mi portafolio!
+      ¡Hola, Bienvenido a mi portafolio ahora si!
     </div>
   );
 }
